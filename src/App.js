8c7a2bfda1a3c94415967a87205bfe107322ffa7
clarsongit <clarson@berkeley.edu>
2023-03-27 13:58:56 -0700

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import WeatherSearch from "./WeatherSearch";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-      </header>
-      <WeatherSearch />
-      <div class="footer">
-        <footer>
-          <a href="https://github.com/clarsongit/react-weather-app">
-            Open-source code
-          </a>{" "}
-          by Chloe Larson
-        </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>React Weather App</h1>
+        </header>
+        <WeatherSearch />
+        <div className="footer">
+          <footer>
+            <a
+              href="https://github.com/clarsongit/react-weather-app"
+              target="_blank"
+            >
+              Open-source code
+            </a>{" "}
+            by Chloe Larson
+          </footer>
+        </div>
       </div>
     </div>
   );
