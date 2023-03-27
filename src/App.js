1659f1f9e76c873879b0498b4c987a0a6715be4f
clarsongit <clarson@berkeley.edu>
2023-03-27 12:49:12 -0700

    Implemented WeatherSearch to search and display weather by city and added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from "react";
+
 import "./App.css";
+import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
@@ -6,6 +9,15 @@ function App() {
       <header className="App-header">
         <h1>React Weather App</h1>
       </header>
+      <WeatherSearch />
+      <div class="footer">
+        <footer>
+          <a href="https://github.com/clarsongit/react-weather-app">
+            Open-source code
+          </a>{" "}
+          by Chloe Larson
+        </footer>
+      </div>
     </div>
   );
 }
